7a16fdfb916354f4fbd09061e58e80ca2e0b67ea
dpolimeni <diego.polimeni99@gmail.com>
2024-03-19 09:41:56 +0100

    add JWT to api

--- pkg/routes/stockRoutes.go
+++ pkg/routes/stockRoutes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"dpolimeni/stockmanagement/app/handlers"
+	"dpolimeni/stockmanagement/pkg/middleware"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -9,6 +10,7 @@ import (
 // StockRoutes sets up the routes for the stock endpoint
 func StockRoutes(app *fiber.App) {
 	route := app.Group("api/v1/stock")
+	route.Use(middleware.JWTMiddleware())
 
 	route.Get("/", handlers.StockHandler)
 }
