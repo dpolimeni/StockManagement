fcd9d1e2d9ede90f20b6ff8a159ffee678f16eb6
dpolimeni <diego.polimeni99@gmail.com>
2024-03-21 20:12:53 +0100

    add restaurant func

--- app/handlers/restaurant.go
+++ app/handlers/restaurant.go
@@ -1,7 +1,21 @@
 package handler
 
 import (
-	"fmt"
-	"github.com/gofiber/fiber/v2"
 	"dpolimeni/stockmanagement/platform/database"
-	
+
+	"github.com/gofiber/fiber/v2"
+)
+
+// RestaurantHandler is the handler for the restaurant endpoint
+// @Summary Add a new restaurant
+// @Description Add a new restaurant to the database
+// @Tags Restaurant
+// @Accept json
+// @Produce json
+// @Param restaurant body schemas.Restaurant true "Restaurant object that needs to be added"
+// @Success 200 {object} schemas.Restaurant
+// @Router /api/v1/restaurant [post]
+// @Param Authorization header string true "Authorization" Default(Bearer )
+func RestaurantHandler(c *fiber.Ctx, DB database.Storage) error {
+
+}
--- platform/database/storage.go
+++ platform/database/storage.go
@@ -1,6 +1,11 @@
 package database
 
+import (
+	"dpolimeni/stockmanagement/app/schemas"
+)
+
 type Storage interface {
 	GetClient() error
 	AuthorizeUser(username, password string) (bool, error)
+	AddRestaurant(restaurant schemas.Restaurant) error
 }
