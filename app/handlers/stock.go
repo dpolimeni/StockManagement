c640910a6b2f360d1f15b7095c94dd7f9bc81388
dpolimeni <diego.polimeni99@gmail.com>
2024-03-22 09:01:26 +0100

    init of other functions to imoleent

--- app/handlers/stock.go
+++ app/handlers/stock.go
@@ -14,3 +14,32 @@ import "github.com/gofiber/fiber/v2"
 func StockHandler(c *fiber.Ctx) error {
 	return c.SendString("Hello, World!")
 }
+
+// Get stock levels
+// This should be called when the stock levels are needed
+func (handler RestaurantHandler) GetStock(c *fiber.Ctx) error {
+	return c.SendString("Hello, World!")
+}
+
+// Initial handler to populate the stock
+func (handler RestaurantHandler) CreateStock(c *fiber.Ctx) error {
+	return c.SendString("Hello, World!")
+}
+
+// Update the stock with product sells
+// This should be called when products are sell
+func (handler RestaurantHandler) SellProduct(c *fiber.Ctx) error {
+	return c.SendString("Hello, World!")
+}
+
+// Update the stock with product purchases
+// This should be called when raw materials are purchased (at every order)
+func (handler RestaurantHandler) PurchaseMaterial(c *fiber.Ctx) error {
+	return c.SendString("Hello, World!")
+}
+
+// Update the stock with real levels
+// This should be called when the stock is updated after a physical count
+func (handler RestaurantHandler) UpdateStock(c *fiber.Ctx) error {
+	return c.SendString("Hello, World!")
+}
